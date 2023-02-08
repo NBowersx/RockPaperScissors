3a686a2cb4ae26fd07d3fe293b655946eae19ace
Nathan <Bowers.Nathanx@gmail.com>
2023-02-08 11:22:07 -0500

    make round funtion to determine winner

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ let computerChoice = Math.floor(Math.random()*3)
  else if (computerChoice === 2){
    computerChoice = "scissors";
  }
+ round(playerChoice,computerChoice);
+console.log("your score "+playerScore +" computers score " +computerScore)
+
+function round(playerChoice,computerChoice)
+    if (playerChoice =='paper' && computerChoice == "rock" || 
+    playerChoice =='rock' && computerChoice == "scissors" || 
+    playerChoice =='scissors' && computerChoice == "paper"){
+        playerScore++
+    }
+    if (computerChoice =='paper' && playerChoice == "rock" || 
+    computerChoice =='rock' && playerChoice == "scissors" || 
+    computerChoice =='scissors' && playerChoice == "paper"){
+    computerScore++
+    }
+else if (playerChoice == computerChoice){
+}
